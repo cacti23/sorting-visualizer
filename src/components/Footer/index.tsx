727629f90fd43317b79fd8cdcbe79e57e408329f
cacti23 <patel.aditya2014@gmail.com>
2022-10-12 16:00:38 +0530

    chore: layout for footer completed

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,37 @@
+import { Box, Flex, HStack, Text, Link } from '@chakra-ui/react'
+
+import { BsGithub, BsLinkedin } from 'react-icons/bs'
+import { FiMonitor } from 'react-icons/fi'
+
 function Footer() {
-  return <div>Footer</div>
+  return (
+    <Box h="10vh">
+      <Flex h="100%" direction="row" justify="center" align="center">
+        <HStack spacing={1}>
+          <Text fontSize="lg">© Aditya Patel.</Text>
+          <HStack spacing={3}>
+            <Link href="https://github.com/cacti23" target="_blank">
+              <BsGithub size={30} />
+            </Link>
+
+            <Link
+              href="https://www.linkedin.com/in/adityap314/"
+              target="_blank"
+            >
+              <BsLinkedin size={30} />
+            </Link>
+
+            <Link
+              href="https://my-portfolio-umber-six.vercel.app/"
+              target="_blank"
+            >
+              <FiMonitor size={30} />
+            </Link>
+          </HStack>
+        </HStack>
+      </Flex>
+    </Box>
+  )
 }
 
 export default Footer
