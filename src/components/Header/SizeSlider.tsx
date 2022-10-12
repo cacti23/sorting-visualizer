8106dde57d26492139e7d29a0901b2cc6a03cc03
cacti23 <patel.aditya2014@gmail.com>
2022-10-12 14:19:10 +0530

    chore: sizeSlider and speedSlider component created with required design

---
+++ src/components/Header/SizeSlider.tsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import {
+  HStack,
+  Slider,
+  SliderTrack,
+  SliderFilledTrack,
+  SliderThumb,
+  Tooltip,
+  Text
+} from '@chakra-ui/react'
+
+function SizeSlider() {
+  const [sliderValue, setSliderValue] = useState(10)
+  const [showTooltip, setShowTooltip] = useState(false)
+
+  return (
+    <HStack spacing={3}>
+      <Text fontSize="md">Size</Text>
+      <Slider
+        aria-label="slider-ex-1"
+        defaultValue={10}
+        min={10}
+        max={300}
+        w="150px"
+        onChange={val => setSliderValue(val)}
+        onMouseEnter={() => setShowTooltip(true)}
+        onMouseLeave={() => setShowTooltip(false)}
+      >
+        <SliderTrack bg="#ccc5b9">
+          <SliderFilledTrack bg="#eb5e28" />
+        </SliderTrack>
+        <Tooltip
+          hasArrow
+          bg="#403d39"
+          color="white"
+          placement="top"
+          isOpen={showTooltip}
+          label={`${sliderValue}`}
+        >
+          <SliderThumb />
+        </Tooltip>
+      </Slider>
+    </HStack>
+  )
+}
+
+export default SizeSlider
